9e749f91a52dbaf83eaa71f85147fadf2805037e
Divya Pandilla <divya.pandilla8@gmail.com>
2017-08-15 18:17:37 +0530

    Modified functions and removed unused imports and functions

--- app/src/androidTest/java/vola/systers/com/volunteers_android/activities/SchedulePageTest.java
+++ app/src/androidTest/java/vola/systers/com/volunteers_android/activities/SchedulePageTest.java
@@ -2,26 +2,30 @@ package vola.systers.com.volunteers_android.activities;
 
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.View;
-
 import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.Description;
 
 import vola.systers.com.volunteers_android.R;
+import vola.systers.com.volunteers_android.model.Event;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
-import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.equalTo;
 import android.support.test.espresso.contrib.NavigationViewActions;
 
 public class SchedulePageTest {
@@ -47,37 +51,35 @@ public class SchedulePageTest {
         };
     }
 
-    private static ViewAction actionCloseDrawer() {
-        return new ViewAction() {
+    private static Matcher<Object> getEventItemName(final Matcher<String> itemMatcher){
+
+        return new BoundedMatcher<Object, Event>(Event.class) {
             @Override
-            public Matcher<View> getConstraints() {
-                return isAssignableFrom(DrawerLayout.class);
+            public void describeTo(org.hamcrest.Description description) {
+                description.appendText("Event with name: ");
+                itemMatcher.describeTo(description);
             }
 
             @Override
-            public String getDescription() {
-                return "close drawer";
-            }
-
-            @Override
-            public void perform(UiController uiController, View view) {
-                ((DrawerLayout) view).closeDrawer(GravityCompat.START);
+            protected boolean matchesSafely(Event event) {
+                return itemMatcher.matches(event.getName());
             }
         };
     }
 
     @Test
-    public void testPerformOnScroll() {
+    public void testPerformOnScroll(){
         onView(withId(R.id.drawer_layout)).perform(actionOpenDrawer());
         onView(withId(R.id.nav_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_schedule));
-        onView(withId(R.id.list)).perform(swipeDown()).check(matches(isDisplayed()));
+        onData(getEventItemName(equalTo
+                ("etouches Test 15FEB"))).inAdapterView(withId(R.id.list)).perform(click());
+
     }
 
     @Test
     public void testOpenNavigationDrawerInSchedulePage() {
         onView(withId(R.id.drawer_layout)).perform(actionOpenDrawer()).check(matches(isDisplayed()));
-
     }
 
 }
